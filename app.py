d485666135f564d1b1828e0984154fc2a4e887f2
jholden13 <gordonchen@outlook.com>
2026-04-23 08:31:06 +0000

    Working App

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ returns = prices.pct_change().iloc[1:]
 names_df = pd.read_csv("names.csv")
 TICKER_NAMES = dict(zip(names_df["Ticker"], names_df["Name"]))
 
-st.title("Stock Correlation Analysis")
-st.caption("Top 30 Stocks - Daily return correlations for last 60 days and 120 days")
+st.title("Stock Correlation")
+st.caption("Top 30 Stocks - Daily return correlation and volatility for last 60 days and 120 days")
 
 tickers = sorted(returns.columns.tolist())
 target = st.selectbox("Pick target stock", tickers)
@@ -50,13 +50,13 @@ chart_6mo = corr_6mo.rename("correlation").to_frame()
 col1, col2 = st.columns(2)
 
 with col1:
-    st.subheader("60 Day Correlation")
+    st.subheader("60 Days")
     st.caption(range_3mo)
     st.dataframe(table_3mo)
  #   st.bar_chart(chart_3mo, horizontal=True, height=400)
 
 with col2:
-    st.subheader("120 Day Correlation")
+    st.subheader("120 Days")
     st.caption(range_6mo)
     st.dataframe(table_6mo)
 #    st.bar_chart(chart_6mo, horizontal=True, height=400)
